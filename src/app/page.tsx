2629cb2674c99854b434ef808ef21e97e2d41369
Vikhyath-vec <vikhyath.kothamasu@gmail.com>
2023-09-16 00:17:51 +0530

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,92 @@
-import Image from 'next/image'
+'use client';
 
-export default function Home() {
+import React, { useState, useEffect } from 'react';
+
+const Page = () => {
+  const [data, setData] = useState([]);
+  const [selectedOption, setSelectedOption] = useState("main gate to hostel circle");
+  const [currentTime, setCurrentTime] = useState("");
+  const [nextResults, setNextResults] = useState([]);
+
+  useEffect(() => {
+    // Fetch data from the JSON file
+    fetch('/bus_schedule.json')
+      .then((response) => response.json())
+      .then((jsonData) => {
+        const selectedData = jsonData[selectedOption];
+        setData(selectedData);
+        updateNextResults(selectedData);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+
+    // Initial call to update time
+    updateTime();
+
+    // Set up an interval to update time every second (1000 milliseconds)
+    const intervalId = setInterval(updateTime, 1000);
+
+    // Cleanup the interval when the component unmounts
+    return () => clearInterval(intervalId);
+  }, [selectedOption]);
+
+  // Function to update the next 10 results
+  const updateNextResults = (selectedData) => {
+    const currentIndex = selectedData.findIndex((time) => time >= currentTime);
+    if (currentIndex === -1) {
+      setNextResults([]);
+    } else {
+      const nextResultsSlice = selectedData.slice(currentIndex, currentIndex + 10);
+      setNextResults(nextResultsSlice);
+    }
+  };
+
+  const updateTime = () => {
+    // Get the current time
+    const now = new Date();
+    const hours = now.getHours().toString().padStart(2, '0');
+    const minutes = now.getMinutes().toString().padStart(2, '0');
+    const seconds = now.getSeconds().toString().padStart(2, '0');
+    setCurrentTime(`${hours}:${minutes}:${seconds}`);
+  };
+
+  const handleOptionChange = (e) => {
+    setSelectedOption(e.target.value);
+    // Update next results when the selected option changes
+    updateNextResults(data);
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <div className="container mx-auto py-8">
+      <h1 className="text-2xl font-bold mb-4 ml-5">Bus Schedule</h1>
+      <div className="mb-4 ml-5">
+        <label htmlFor="busOptions" className="mr-5">
+          Select an option:
+        </label>
+        <select
+          id="busOptions"
+          value={selectedOption}
+          onChange={handleOptionChange}
+          className="bg-blue-100 text-blue-800 border-blue-500 rounded-md px-2 py-1 focus:outline-none focus:ring focus:border-blue-300"
+        >
+           <option value="main gate to hostel circle">Main Gate to Hostel Circle</option>
+          <option value="hostel circle to main gate">Hostel Circle to Main Gate</option>
+          <option value="hostel circle to new hostels">Hostel Circle to New Hostels</option>
+          <option value="new hostels to hostel circle">New Hostels to Hostel Circle</option>
+          <option value="hospital to trp">Hospital to TRP</option>
+        </select>
       </div>
+      <h2 className="text-xl font-semibold mb-2 ml-5">Selected Path: {selectedOption}</h2>
+      <p className="ml-5">Current Time: {currentTime}</p>
+      <h2 className="text-xl font-semibold mb-2 ml-5">Timings of next 10 Buses:</h2>
+      <ul className="ml-5">
+        {nextResults.map((time, index) => (
+          <li key={index}>{time}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
-}
+export default Page;
